0f010deadc26752f1be920dc94f1cabb91dca777
imen <imen.guidara17@gmail.com>
2025-10-02 23:06:44 -0700

    changes

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -24,14 +24,13 @@ export function ContactPage() {
     if (res.ok) alert("Message envoyé !");
     setFormData({ name: "", email: "", phone: "", subject: "", message: "" });
   };
-
   return (
     <div className="min-h-screen bg-white flex items-center justify-center ">
       <div className="max-w-6xl w-full grid grid-cols-1 md:grid-cols-2 gap-10">
         
         {/*formulaire */}
         <div className="bg-white p-10 rounded-3xl shadow-2xl">
-          <h2 className="text-3xl font-bold mb-6 text-indigo-700">Get in Touch!</h2>
+          <h2 className="text-3xl font-bold mb-6 text-indigo-700">Contactez Nous!</h2>
           <form onSubmit={handleSubmit} className="flex flex-col gap-5">
             <input type="text" name="name" placeholder="Nom" value={formData.name} onChange={handleChange} className="border border-gray-300 p-3 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-400 transition" required />
             <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} className="border border-gray-300 p-3 rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-400 transition" required />
@@ -42,7 +41,7 @@ export function ContactPage() {
               <option value="Marketing">Marketing</option>
               <option value="Marketing">Site Vitrine</option>
               <option value="Marketing">Site E-Commerce</option>
-              <option value="Other">Other</option>
+              <option value="Other">Autre</option>
             </select>
             <textarea name="message" placeholder="Message" value={formData.message} onChange={handleChange} className="border border-gray-300 p-3 rounded-xl h-32 resize-none focus:outline-none focus:ring-2 focus:ring-indigo-400 transition" required />
             <button type="submit" className="bg-indigo-600 text-white px-6 py-3 rounded-xl font-semibold hover:bg-indigo-700 transition transform hover:scale-105">Envoyer</button>
@@ -89,7 +88,7 @@ export function ContactPage() {
           <div>
             <h3 className="text-lg font-semibold text-indigo-700 mb-2">Nos Horaires</h3>
             <p>Lundi – Vendredi: 9:00 H – 18:00 H</p>
-            <p>Samedi: 10:00 H – 14H:00 H</p>
+            <p>Samedi: 10:00 H – 14:00 H</p>
             <p>Dimanche: Fermée</p>
           </div>
         </div>
